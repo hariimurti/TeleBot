22a5cd458bcd12f0ec0f8c63b1144fdbf87b3d33
Harimurti <hariimurti@gmail.com>
2018-04-23 22:22:21 +0700

    classes: extension: konverter ukuran file

--- TeleBot/Classes/Extension.cs
+++ TeleBot/Classes/Extension.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Text.RegularExpressions;
 using TeleBot.BotClient;
 
@@ -50,6 +51,38 @@ namespace TeleBot.Classes
         public static string UrlDecode(this string text)
         {
             return WebUtility.UrlDecode(text);
+        }
+
+        public static string ToHumanSizeFormat(this double value)
+        {
+            string[] suffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+            for (int i = 0; i < suffixes.Length; i++)
+            {
+                if (value <= (Math.Pow(1024, i + 1)))
+                {
+                    return ThreeNonZeroDigits(value / Math.Pow(1024, i)) + " " + suffixes[i];
+                }
+            }
+            return ThreeNonZeroDigits(value / Math.Pow(1024, suffixes.Length - 1)) + " " + suffixes[suffixes.Length - 1];
+        }
+        
+        private static string ThreeNonZeroDigits(double value)
+        {
+            if (value >= 100)
+            {
+                // No digits after the decimal.
+                return value.ToString("0,0");
+            }
+            else if (value >= 10)
+            {
+                // One digit after the decimal.
+                return value.ToString("0.0");
+            }
+            else
+            {
+                // Two digits after the decimal.
+                return value.ToString("0.00");
+            }
         }
     }
 }
