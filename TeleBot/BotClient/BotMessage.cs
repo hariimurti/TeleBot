e19eb39f1f731412508f2cb3b7d1004b56d2904d
Harimurti <hariimurti@gmail.com>
2018-04-23 14:24:13 +0700

    callbackquery: antisipasi flood pencet tombol

--- TeleBot/BotClient/BotMessage.cs
+++ TeleBot/BotClient/BotMessage.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Timers;
 using TeleBot.Classes;
 using TeleBot.Plugins;
 using TeleBot.SQLite;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace TeleBot.BotClient
@@ -12,6 +17,16 @@ namespace TeleBot.BotClient
     {
         private static Log _log = new Log("Message");
         private static Database _db = new Database();
+        private static List<CallbackData> _callbackBlocked = new List<CallbackData>();
+        private static readonly int _callbackBlockedTimeout = 60;
+            
+        private class CallbackData
+        {
+            public int MessageId { get; set; }
+            public int FromId { get; set; }
+            public string Info { get; set; }
+            public DateTime LastTime { get; set; }
+        }
         
         public static async void OnMessage(object sender, MessageEventArgs e)
         {
@@ -102,6 +117,7 @@ namespace TeleBot.BotClient
                     break;
                 
                 case "call":
+                    if (await IsCallbackBlocked(callback, "Hashtag")) return;
                     new Bookmark(message, callback).FindHashtag(data);
                     break;
                 
@@ -126,6 +142,7 @@ namespace TeleBot.BotClient
                     break;
                 
                 case "mobilism":
+                    if (await IsCallbackBlocked(callback, "Mobilism")) return;
                     new Mobilism(message, callback).OpenThread(data);
                     break;
                 
@@ -134,6 +151,56 @@ namespace TeleBot.BotClient
                     await Bot.AnswerCallbackQueryAsync(callback.Id, "Perintah tidak diketahui", true);
                     break;
             }
+        }
+
+        private static async Task<bool> IsCallbackBlocked(CallbackQuery callback, string info)
+        {
+            var message = callback.Message;
+            var found = _callbackBlocked.
+                Where(c => c.MessageId == message.MessageId && c.FromId == callback.From.Id).
+                ToList().FirstOrDefault();
+            
+            if (found != null)
+            {
+                var countdown = (DateTime.Now - found.LastTime).Seconds;
+                if (countdown < _callbackBlockedTimeout)
+                {
+                    await Bot.AnswerCallbackQueryAsync(callback.Id,
+                        $"Sabar ya kaka...\nTunggu {_callbackBlockedTimeout - countdown} dtk baru bisa tekan tombol ini lagi.", true);
+                    
+                    return true;
+                }
+
+                return false;
+            }
+            else
+            {
+                _log.Debug("Block Callback: {0}: {1} -- {2}", info, message.MessageId, callback.From.Id);
+                
+                var callbackData = new CallbackData()
+                {
+                    MessageId = message.MessageId,
+                    FromId = callback.From.Id,
+                    Info = info,
+                    LastTime = DateTime.Now
+                };
+                
+                _callbackBlocked.Add(callbackData);
+                
+                var timer = new Timer(_callbackBlockedTimeout * 1000);
+                timer.Elapsed += (ts, te) => TimerElapsed(timer, callbackData);
+                timer.Start();
+                return false;
+            }
+        }
+        
+        private static void TimerElapsed(Timer timer, CallbackData data)
+        {
+            timer?.Stop();
+            timer?.Dispose();
+
+            _log.Debug("Unblock Callback: {0}: {1} -- {2}", data.Info, data.MessageId, data.FromId);
+            _callbackBlocked.Remove(data);
         }
     }
 }
