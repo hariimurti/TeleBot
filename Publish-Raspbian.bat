f858254fbdbd46753d6c85d9d8887f629ec41ce6
Harimurti <hariimurti@gmail.com>
2018-04-24 10:47:08 +0700

    publish: upload via ftp
    aplikasi yg dibutuhkan:
    - ftp
    - putty

--- Publish-Raspbian.bat
+++ Publish-Raspbian.bat
@@ -1,4 +1,59 @@
-@echo off
-cd TeleBot
-dotnet publish -c Release -r linux-arm -o bin/Raspbian
-timeout 5
+@ECHO off
+ECHO TeleBot Publisher
+ECHO.
+
+REM Connection information:
+SET Server=192.168.1.21
+SET Username=pi
+SET Password=ok
+SET ProjectDir=TeleBot
+SET BuildDir=bin\Raspbian
+SET UploadTo=/opt/TeleBot
+
+REM Build project...
+CD %ProjectDir%
+dotnet publish -c Release -r linux-arm -o %BuildDir%
+
+ECHO.
+ECHO Upload into Raspbian?
+PAUSE
+
+CD ..
+ECHO Prepare SSH...
+SET SSHCommands="ssh_commands.txt"
+
+ECHO.
+ECHO SSH: Stoping TeleBot Service...
+ECHO sudo systemctl stop telebot> %SSHCommands%
+putty.exe -ssh %Username%@%Server% -pw %Password% -m %SSHCommands%
+
+ECHO.
+ECHO Prepare FTP...
+SET FTPCommands="ftp_commands.txt"
+ECHO open %Server%> %FTPCommands%
+ECHO %Username%>> %FTPCommands%
+ECHO %Password%>> %FTPCommands%
+ECHO binary>> %FTPCommands%
+ECHO cd %UploadTo%>> %FTPCommands%
+ECHO prompt>> %FTPCommands%
+ECHO mdel *>> %FTPCommands%
+ECHO mput "%~dp0%ProjectDir%\%BuildDir%\*">> %FTPCommands%
+ECHO quit>> %FTPCommands%
+
+ECHO.
+ECHO Upload via FTP...
+ftp -s:%FTPCommands%
+
+ECHO.
+ECHO SSH: Starting TeleBot Service...
+ECHO chmod +x %UploadTo%/TeleBot> %SSHCommands%
+ECHO sudo systemctl start telebot>> %SSHCommands%
+putty.exe -ssh %Username%@%Server% -pw %Password% -m %SSHCommands%
+
+ECHO.
+ECHO Clean up...
+IF EXIST %SSHCommands% DEL %SSHCommands%
+IF EXIST %FTPCommands% DEL %FTPCommands%
+
+ECHO.
+PAUSE
